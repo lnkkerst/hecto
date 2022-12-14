36e213f53832b090316bce723d44e571bd6fad93
lnkkerst <lnkkerst04@gmail.com>
2022-12-14 22:41:25 +0800

    feat: save inplace

--- src/document.rs
+++ src/document.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, io::Write};
 
 use crate::{Position, Row};
 
@@ -33,7 +33,23 @@ impl Document {
         self.rows.len()
     }
 
+    fn insert_newline(&mut self, at: &Position) {
+        if at.y > self.len() {
+            return;
+        }
+        if at.y == self.len() {
+            self.rows.push(Row::default());
+            return;
+        }
+        let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
+        self.rows.insert(at.y + 1, new_row);
+    }
+
     pub fn insert(&mut self, at: &Position, c: char) {
+        if c == '\n' {
+            self.insert_newline(at);
+            return;
+        }
         if at.y == self.len() {
             let mut row = Row::default();
             row.insert(0, c);
@@ -42,5 +58,31 @@ impl Document {
             let row = self.rows.get_mut(at.y).unwrap();
             row.insert(at.x, c);
         }
+    }
+
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y > len {
+            return;
+        }
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
+        }
+    }
+
+    pub fn save(&self) -> Result<(), std::io::Error> {
+        if let Some(file_name) = &self.file_name {
+            let mut file = fs::File::create(file_name)?;
+            for row in &self.rows {
+                file.write_all(row.as_bytes())?;
+                file.write_all(b"\n")?;
+            }
+        }
+        Ok(())
     }
 }
