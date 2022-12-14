4432fa9c7fca7bb5275ea3e26eecafb7c3fb8c90
lnkkerst <lnkkerst04@gmail.com>
2022-12-14 20:13:44 +0800

    feat: view text

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,6 @@
-use crate::Terminal;
+use std::env;
+
+use crate::{Document, Row, Terminal};
 use crossterm::{
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     terminal::enable_raw_mode,
@@ -6,7 +8,7 @@ use crossterm::{
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -17,6 +19,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -38,16 +41,25 @@ impl Editor {
 
     pub fn default() -> Self {
         enable_raw_mode().unwrap();
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
         Self {
             should_quit: false,
             terminal: Terminal::defalut().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
+            document,
         }
     }
 
     fn refresh_screen(&self) -> crossterm::Result<()> {
         Terminal::cursor_hide()?;
-        Terminal::cursor_position(&Position { x: 0, y: 0 })?;
+        Terminal::cursor_position(&Position::default())?;
 
         if self.should_quit {
             Terminal::clear_screen()?;
@@ -132,11 +144,20 @@ impl Editor {
         println!("{}\r", welcome_message);
     }
 
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row);
+    }
+
     fn draw_rows(&self) -> crossterm::Result<()> {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line()?;
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
